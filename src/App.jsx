1716bdf5ae067ff8fb1a8490f43e207e5cd9d7b7
santosh pradhan <saank980@gmail.com>
2025-04-29 10:02:07 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,43 @@
+import { useEffect } from 'react';
 import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return <div>
-  <h2>hii</h2>
+  
     <Counter></Counter>
+
+
+
     </div>
   
 }
 
 function Counter() {
-
-
   const [count,setCount] = useState(0)
+  console.log("counter");
   
+  useEffect(function () {
+      setInterval(() => {
+    setCount(count =>count+1);
+    // setCount(function(count){
+    //return count +1})
+    
+  }, 1000);
+  console.log("mount");
+  },[])
+
+
   function increaseCount(){
 setCount(count+1);
   }
 
-function decreaseCount() {
-  setCount(count-1);
-}
-function resetCount() {
-  setCount(0);
-}
 
   return <div>
     <h1>{count}</h1>
     <button onClick={increaseCount}>increase count </button>
-    <button onClick={decreaseCount}>decrease count</button>
-    <button onClick={resetCount}>reset count</button>
+
   </div>
 }
 export default App
